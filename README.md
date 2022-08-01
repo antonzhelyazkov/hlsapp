6fcd37e42c05f2519f07cb84316581f805c421f2
root <root@helloworld.localdomain>
2022-08-01 10:24:48 +0300

    ffmpeg 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # htlspp
 api mp4 to hls
+
+## ffmpeg compile script. Works on Centos7/AlmaLinux8/RockyLimux8
+
+```res/ffmpeg_compile.sh```
--- resources/ffmpeg_compile.sh
+++ resources/ffmpeg_compile.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+rpms=("epel-release" "autoconf" "automake" "bzip2" "bzip2-devel" "cmake" "freetype-devel" "gcc" "gcc-c++" "git" "libtool" "make" "pkgconfig" "zlib-devel")
+
+for rpm in ${rpms[@]}
+do
+    if rpm -q --quiet $rpm
+    then 
+        echo $rpm "alredy installed"
+    else
+        echo "install $rpm"
+        yum install -y $rpm
+    fi
+done
